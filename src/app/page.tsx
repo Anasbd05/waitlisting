5efcc93765c0e156cdf1715e01a8055eabf314ef
anas bd <anastrying05@gmail.com>
2025-06-16 13:34:57 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
+"use client"
+import {supabase} from "@/supabase/client";
+import {LoaderPinwheel} from "lucide-react";
 import Image from "next/image";
+import {useState} from "react";
+import {toast} from "sonner";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+  const [email,setEmail] = useState('')
+  const [loading,setLoading] = useState(false)
+
+  const insertEmail = async () => {
+
+    if(email.trim() === "") {
+      toast.error("Fill the email input to join the waitlist");
+      return;  // stop execution if empty
+    }
+    try {
+      setLoading(true)
+      const {error} = await supabase
+        .from('emails')
+        .insert({email: email})
+
+      if(error) {
+        toast.error(error.message);
+      } else {
+        toast.success("Success!",{
+          description: "You’ve been added to our waitlist. Stay tuned — we’ll be in touch soon!",
+        });
+      }
+
+    } catch(error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+      setEmail('');
+    }
+  }
+
+
+  return (
+    <section className="overflow-x-clip h-screen px-4 w-full bg-gradient-to-br from-white to-[#9e98c8] py-20">
+      <main className="flex items-center flex-col justify-center w-full">
+        <div className="flex gap-1 items-center">
+          <Image src={"/logo.png"} width={40} height={40} alt="logo image" />
+          <h1 className="text-accent text-2xl font-bold tracking-wide ">RapidAds</h1>
         </div>
+        <p className="mt-8 max-w-2xl leading-tight text-3xl sm:text-4xl lg:text-5xl font-bold text-center text-accent">
+          We&#39;d love you to be apart of something big
+        </p>
+
+        <main className="px-5 py-8 rounded-lg mt-10 bg-gradient-to-br from-white to-[#cfcaee]">
+          <h1 className="text-xl md:text-2xl text-center font-semibold">Join the waitlist</h1>
+          <p className="text-start mt-2 max-w-xl font-medium ">
+            to get early access, exclusive updates, and special rewards — directly in your inbox.
+          </p>
+
+          <div className="p-2 bg-gray-100 rounded-md mt-4">
+            <div className="p-2 rounded-md bg-white flex flex-col md:flex-row gap-2 items-center">
+              <input value={email} onChange={(e) => setEmail(e.target.value)} className="px-2 md:py-1 py-2 border shadow-sm md:shadow-none md:border-none border-gray-200 rounded-md outline-none w-full" placeholder="Your email address" />
+              <button onClick={insertEmail} className="px-6 py-2 rounded-md w-full font-medium tracking-wide md:w-36 bg-indigo-500 text-white">
+                {loading ? <span className="flex gap-2 items-center"> <LoaderPinwheel className="animate-spin h-4 w-4" /> Subscribe</span> : <span>Subscribe</span>}
+              </button>
+            </div>
+          </div>
+
+
+        </main>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+    </section>
   );
 }
